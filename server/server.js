da03a11984101f2af5d1a0ef81cff9566b01215a
Santi <santicorapollo@gmail.com>
2026-09-24 00:00:09 -0500

    perf(server): anadir header Access-Control-Max-Age para preflights cors en webview2

--- server/server.js
+++ server/server.js
@@ -87,6 +87,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Max-Age', '86400');
   }
 
   if (req.method === 'OPTIONS') {
